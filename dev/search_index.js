ad9ef58393dc15648fc3395f230d6a6b02681dca
Documenter.jl <documenter@juliadocs.github.io>
2022-09-16 20:39:55 +0000

    build based on 03887da

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ConvolutionKernel","category":"page"},{"location":"#ConvolutionKernel","page":"Home","title":"ConvolutionKernel","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ConvolutionKernel.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ConvolutionKernel]","category":"page"}]
+}
